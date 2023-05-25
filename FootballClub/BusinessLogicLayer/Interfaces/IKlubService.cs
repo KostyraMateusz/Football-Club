2b10e090bdd1fd08edf1a6b00b43b4d212bd2860
StaKluczewski <72356701+StaKluczewski@users.noreply.github.com>
2023-05-25 19:09:13 +0200

    Implementacja testów MVC dla klubu

--- FootballClub/BusinessLogicLayer/Interfaces/IKlubService.cs
+++ FootballClub/BusinessLogicLayer/Interfaces/IKlubService.cs
@@ -12,11 +12,11 @@ namespace BusinessLogicLayer.Interfaces
         Task<IEnumerable<Klub>> DajKluby();
         Task<IEnumerable<Pilkarz>> DajArchiwalnychPilkarzy(Guid IdKlubu);
         Task<Pilkarz> DajArchiwalnegoPilkarza(Guid IdKlubu, Guid IdPilkarza);
-        Task <IEnumerable<Pilkarz>> DajObecnychPilkarzy(Guid IdKlubu);
+        Task <IEnumerable<Pilkarz>> DajObecnychPilkarzy(Klub klub);
         Task<Pilkarz> DajObecnegoPilkarza(Guid IdKlubu, Guid IdPilkarza);
-        Task<string> DajStadionKlubu(Guid IdKlubu);
-        Task<string> DajTrofeaKlubu(Guid IdKlubu);
-        Task DodajPilkarzaDoObecnych(Pilkarz pilkarz, Guid IdKlubu);
+        Task<string> DajStadionKlubu(Klub klub);
+        Task<string> DajTrofeaKlubu(Klub klub);
+        Task DodajPilkarzaDoObecnych(Pilkarz pilkarz, Klub klub);
         Task UsunPilkarzaZObecnych(Guid PilkarzId, Guid IdKlubu);
         Task DodajPilkarzyDoObecnych(List<Pilkarz> pilkarze, Klub klub);
     }
--- FootballClub/TestFootballClub/ControllerTests/Kluby/KlubyControllerMVCTest.cs
+++ FootballClub/TestFootballClub/ControllerTests/Kluby/KlubyControllerMVCTest.cs
@@ -1,4 +1,5 @@
 ﻿using BusinessLogicLayer.Interfaces;
+using FootballClubLibrary.Models;
 using FootballClubPresentationLayer.ControllersMVC;
 using FootballClubWeb.Controllers;
 using Microsoft.AspNetCore.Mvc;
@@ -38,32 +39,65 @@ namespace TestsFootballClub.ControllerTests.Kluby
         }
 
         [Fact]
+        public async Task TestDajObecnychPilkarzyKlubu()
+        {
+            // Arrange
+            Mock<IKlubService> mockKlubyService = new Mock<IKlubService>();
+            var klub = new Klub()
+            {
+                IdKlub = Guid.NewGuid(),
+                Nazwa = "Klub1",
+                ObecniPilkarze = new List<Pilkarz> {
+                    new Pilkarz() { IdPilkarz = Guid.NewGuid(), Imie = "Jan", Nazwisko = "Nowak" },
+                    new Pilkarz() { IdPilkarz = Guid.NewGuid(), Imie = "Tomek", Nazwisko = "Januszek" }
+                }
+            };
+            mockKlubyService.Setup(x => x.DajObecnychPilkarzy(klub))
+                .ReturnsAsync(klub.ObecniPilkarze);
+            var klubyController = new KlubyControllerMVC(mockKlubyService.Object);
+
+            // Act
+            var result = await Task.FromResult(klubyController.DajObecnychPilkarzyKlubu(klub));
+            var viewResult = (ViewResult)result;
+
+            // Assert
+            Assert.IsType<ViewResult>(viewResult);
+            Assert.Equal(klub.ObecniPilkarze, viewResult.ViewData["Pilkarze"]);
+        }
+
+        [Fact]
         public async Task TestDodajPilkarzaDoObecnych()
         {
             // Arrange
             var klubMockService = new KlubServiceMock();
             var klubController = new KlubyControllerMVC(klubMockService);
             var pilkarz = new Pilkarz() { IdPilkarz = Guid.NewGuid(), Imie = "Piotr", Nazwisko = "Nowak" };
-            var klub = new Klub() { IdKlub = Guid.NewGuid(), Nazwa = "Klub1" };
+            var klub = new Klub() { IdKlub = Guid.NewGuid(), Nazwa = "Klub1", ObecniPilkarze = new List<Pilkarz> { } };
 
             // Act
-            var result = klubController.DodajPilkarzaDoObecnych(klub.IdKlub, pilkarz);
+            var result = klubController.DodajPilkarzaDoObecnych(klub, pilkarz);
             var resultView = (ViewResult)result;
 
             // Assert
-            Assert.IsType<ViewResult>(result);
-            Assert.Equal(1, klubMockService.DajObecnychPilkarzy(klub.IdKlub).Result.Count());
+            Assert.IsType<ViewResult>(resultView);
+            Assert.Equal(1, klubMockService.DajObecnychPilkarzy(klub).Result.Count());
         }
 
         [Fact]
-        public async Task TestDajTrofeaKlubu()
+        public async Task TestDajStadionKlubu()
         {
             // Arrange
-            Mock<IKlubService> mockKlubyService = new Mock<IKlubService>();
-        
+            var klubMockService = new KlubServiceMock();
+            var klubController = new KlubyControllerMVC(klubMockService);
+            var klub = new Klub() { IdKlub = Guid.NewGuid(), Nazwa = "Atletico Madryt", Stadion = "Wanda Metropolitano" };
+
             // Act
+            var result = await Task.FromResult(klubController.DajStadion(klub));
+            var resultView = (ViewResult)result;
 
             // Assert
+            Assert.IsType<ViewResult>(resultView);
+            Assert.Equal(klub.Stadion, resultView.ViewData["Stadion"]);
 
         }
     }
